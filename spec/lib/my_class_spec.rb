61bfc913ffec0806e1ee383792cd6032290accf9
Gustavo Canedo <canedo@webgoal.com.br>
2017-12-02 17:58:03 -0200

    atualiza status inicial do teste

--- spec/lib/my_class_spec.rb
+++ spec/lib/my_class_spec.rb
@@ -1,7 +1,7 @@
 describe MyClass do
   describe 'the_truth' do
     it 'deve retornar true' do
-      expect(MyClass.the_truth).to eq true
+      expect(MyClass.the_truth).to eq false
     end
   end
 end
